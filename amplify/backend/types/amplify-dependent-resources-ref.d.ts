63bb7b9bf7752c81a0b6c08a910316750aa626a5
Kingsley Okeke <nkokeke@ualberta.ca>
2022-03-20 21:23:05 -0600

    Artics

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifiedchatapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
